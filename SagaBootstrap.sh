4b9249effcb241750bf5b796bf9d18e9ff9ef2d0
mangae <37798589+mangae@users.noreply.github.com>
2018-12-06 17:52:18 +0100

    Update SagaBootstrap.sh

--- SagaBootstrap.sh
+++ SagaBootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
 sagacoind stop 
+sleep 20
 cd Sagacoin_mangae/
 cat bootstrap.dat.gz.00* > bootstrap.dat.gz
 gunzip bootstrap.dat.gz
@@ -11,4 +12,5 @@ sagacoind -loadblock=bootstrap.dat
 cd 
 #sagacoind -daemon 
 sagacoind getinfo 
+sleep 20
 echo "Bootstrap loaded successfully"
