eac97402c16bcdffeba37b7eb3d28cfa6e21787e
mangae <37798589+mangae@users.noreply.github.com>
2019-05-08 22:38:06 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 sudo apt-get install unzip -y
 
 cd Sagacoin_mangae/
-cat bootstrap.zip.gz.00* > bootstrap.zip
+wget https://www.dropbox.com/s/uz8ot0oj73a1mt6/bootstrap.zip?dl=0
 cp bootstrap.zip /root/.SagaCoin/bootstrap.zip
 killall -9 sagacoind
 
