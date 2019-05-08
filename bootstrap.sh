1895d10284d747a2b7937305f9e74cb049f55ca6
mangae <37798589+mangae@users.noreply.github.com>
2019-05-08 14:14:45 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
+
 sudo apt-get unzip -y
+
+cd Sagacoin_mangae/
+cat bootstrap.zip.gz.00* > bootstrap.zip
+cp bootstrap.zip /root/.SagaCoin
 killall -9 sagacoind
+
 cd /root/.SagaCoin
-
-
 rm -R ./database &>/dev/null &
-rm -R ./smsgStore	&>/dev/null &
+rm -R ./smsgStore &>/dev/null &
 rm -R ./smsgDB	&>/dev/null &
 rm -R ./txleveldb &>/dev/null &
 rm  blk0001.dat &>/dev/null &
+
 unzip  bootstrap.zip
 rm ./bootstrap.zip
-cd
+rm -r /root/Sagacoin_mangae
+sleep 5
+echo Starting SagaCoin
 sagacoind -daemon
+sleep 20
+sagacoind getinfo
+echo Bootstrap completed
